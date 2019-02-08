a94a1a575d46d6c248dfba33948ed5f567e825ae
Stuart <stuart@yamartino.com>
2019-02-08 15:28:23 -0500

    first check that the navigator exists

--- dist/simple-react-validator.js
+++ dist/simple-react-validator.js
@@ -388,7 +388,7 @@ function () {
       };
     } else if (_options.hasOwnProperty('element')) {
       this.element = _options.element;
-    } else if (navigator.product === "ReactNative") {
+    } else if ((typeof navigator === "undefined" ? "undefined" : _typeof(navigator)) === 'object' && navigator.product === 'ReactNative') {
       this.element = function (message) {
         return message;
       };
