bc9817b894e79742e8006545028a2796fc849b5e
Stuart <stuart@yamartino.com>
2020-01-13 10:02:25 -0500

    Turkish 1.4.2 release

---
+++ dist/locale/tr.js
@@ -0,0 +1,51 @@
+// Simple React Validator v1.4.2 | Created By Dockwa | MIT License | 2017 - Present
+;(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['simple-react-validator'], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('simple-react-validator'));
+  } else {
+    root.SimpleReactValidatorLocaleTr = factory(root.SimpleReactValidator);
+  }
+}(this, function(SimpleReactValidator) {
+"use strict";
+
+// Turkish
+SimpleReactValidator.addLocale('tr', {
+  accepted: ':attribute onaylanmalıdır.',
+  after: ':attribute :date tarihinden sonra olmalıdır.',
+  after_or_equal: ':attribute :date tarihinde veya sonra olmalıdır.',
+  alpha: ':attribute sadece harf içerebilir.',
+  alpha_space: ':attribute sadece harf, ve boşluk karakteri içerebilir.',
+  alpha_num: ':attribute sadece harf, ve rakam içerebilir.',
+  alpha_num_space: ':attribute sadece harf, rakam, ve boşluk karakteri içerebilir.',
+  alpha_num_dash: ':attribute sadece harf, rakam, ve tire karakteri içerebilir.',
+  alpha_num_dash_space: ':attribute sadece harf, rakam, tire, ve boşluk karakteri içerebilir.',
+  array: ':attribute dizi tipinde olmalıdır.',
+  before: ':attribute :date tarihinden önce olmalıdır.',
+  before_or_equal: ':attribute :date tarihinde veya daha önce olmalıdır.',
+  between: ':attribute  :min - :max:type aralığında olmalıdır.',
+  "boolean": ':attribute geçerli bir mantıksal değer olmalıdır.',
+  card_exp: ':attribute geçerli bir sonlanma tarihi olmalıdır.',
+  card_num: ':attribute geçerli bir kredi kartı numarası olmalıdır.',
+  currency: ':attribute geçerli bir para tipi olmalıdır.',
+  date: ':attribute tarih formatında olmalıdır.',
+  date_equals: ':attribute :date tarihli olmalıdır.',
+  email: ':attribute geçerli bir e-posta adresi olmalıdır.',
+  "in": 'Seçilen :attribute :values olmalıdır.',
+  integer: ':attribute sayı olmalıdır.',
+  max: ':attribute :max:type dan büyük olamaz.',
+  min: ':attribute en az :min:type olmalıdır.',
+  not_in: 'Seçilen :attribute :values olamaz.',
+  not_regex: ':attribute gerekli formattan farklı olmalıdır.',
+  numeric: ':attribute sayı olmalıdır.',
+  phone: ':attribute geçerli bir telefon numarası olmalıdır.',
+  regex: ':attribute gerekli formatla uyumlu olmalıdır.',
+  required: ':attribute alanı doldurulmalıdır.',
+  size: ':attribute :size:type tipinde olmalıdır.',
+  string: ':attribute karakter dizisi olmalıdır.',
+  "typeof": ':attribute :type tipinde olmalıdır.',
+  url: ':attribute url olmalıdır.'
+});
+return null;
+}));
--- dist/simple-react-validator.js
+++ dist/simple-react-validator.js
@@ -1,4 +1,4 @@
-// Simple React Validator v1.4.1 | Created By Dockwa | MIT License | 2017 - Present
+// Simple React Validator v1.4.2 | Created By Dockwa | MIT License | 2017 - Present
 ;(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['react'], factory);
